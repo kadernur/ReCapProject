12fff306768dbe6bdac1d5f5e16b46b91f235849
kaderNurTekin <tekinkadernur@gmail.com>
2021-03-03 21:24:06 +0300

    12.gün 3.ödev

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,6 +1,9 @@
 ﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFrameWork;
 using System;
@@ -29,6 +32,13 @@ namespace Business.DependencyResolvers.Autofac
             builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
 
 
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions() {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
 
         }
     }
